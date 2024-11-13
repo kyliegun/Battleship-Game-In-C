99c0abe4c1aeba25fccf95d4711839e3b98c04b1
Lukas_Buehlmann <sakulb05@gmail.com>
2024-11-13 12:48:40 -0500

    added prototypes to game.h

--- game.c
+++ game.c
@@ -1,1 +1,11 @@
-//nothing here yet
+#include "game.h"
+
+//generates the ships on a board of dimensions HEIGHT X WIDTH (maybe define number of ships too)
+void generateBoard(int shipBoard[][WIDTH]) {
+    // add ships by modifying board
+}
+
+// prints both boards to stdout
+void drawBoard(int shipBoard[][WIDTH], int shotBoard[][WIDTH]) {
+    // print board contents to stdout
+}
--- game.h
+++ game.h
@@ -1,3 +1,5 @@
 #define WIDTH 10
 #define HEIGHT 10
 
+void generateBoard(int shipBoard[][WIDTH]);
+void drawBoard(int shipBoard[][WIDTH], int shotBoard[][WIDTH]);
