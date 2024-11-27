f821c0bee18f35efa0e6ca4ac348124f123efb1e
Lukas_Buehlmann <sakulb05@gmail.com>
2024-11-27 11:37:27 -0500

    made basic opponent difficulty

--- opponentAI.h
+++ opponentAI.h
@@ -1,1 +1,12 @@
-//nothing
+#include "game.h"
+
+struct pos{
+    int x;
+    int y;
+};
+typedef struct pos Pos;
+
+void opponentShoot(int *x, int *y, int difficulty, int shotBoard[][WIDTH]);
+Pos *freeSpace(int shotBoard[][WIDTH], int *size);
+
+void easyMode(int *x, int *y, int shotBoard[][WIDTH]);
