e5ea4f6904119fde68f6df60482e81c643476e8e
yalexisq <ysabella.a.quilatan@gmail.com>
2024-11-26 19:28:21 -0500

    Updated game.c file

--- game.c
+++ game.c
@@ -4,8 +4,28 @@
 #include <wchar.h>
 #include <locale.h>
 
+/*
+ * Parameter names listed here are (from perspective of the player):
+ *  shipBoard[][] - your own integer array of ship positions
+ *  shotBoard[][] - integer array that tracks the hits, misses, and available places to shoot
+ *  targetBoard[][] - the opponents ship position array
+ *  opponentShots[][] - the opponent's shotBoard
+ * 
+ * All arrays (so far) are modified as a side effect of the function. No arrays are returned
+ * 
+ * shipBoard is a 2D array that represents the board. Each ship is represented as a line of integers
+ * with their own ID. For example, if there are six ships, then ship one would be the collection of
+ * 1 elements, ship two as 2s, etc. Each player has one
+ * 
+ * shotBoard is a 2D array of integers where 0 is a spot that hasn't been tried yet, -1 is a miss, 
+ * 1 is a hit. Each player has one
+*/
+
+//generates the ships on a board of dimensions HEIGHT X WIDTH (maybe define number of ships too)
+// from our development plan, only has to work for a 10x10 to start
 void generateBoard(int shipBoard[][WIDTH], Ship ships[]) {
-    //Place ships at random positions
+    // add ships by modifying board
+    // Initialize the shipboard with 0s (empty)
     for (int i = 0; i < NUM_SHIPS; i++) {
         int x = rand() % WIDTH;
         int y = rand() % HEIGHT;
@@ -17,6 +37,7 @@ void generateBoard(int shipBoard[][WIDTH], Ship ships[]) {
     }
 }
 
+// prints both boards to stdout
 void drawBoard(int shipBoard[][WIDTH], int shotBoard[][WIDTH], int opponentBoard[][WIDTH], int opponentShots[][WIDTH]) {
     setlocale(LC_ALL, "");
 
@@ -56,6 +77,11 @@ void drawBoard(int shipBoard[][WIDTH], int shotBoard[][WIDTH], int opponentBoard
     printf("  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾  |    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ \n");
 }
 
+/*
+ * Shoots at position given by y, x on targetBoard.
+ * Places result onto shotBoard as 1 for hit and -1 for miss.
+ * Returns a 0 for miss, 1 for hit, or 2 for hit and sunk (uses isSunk for this check)
+*/
 int shoot(int x, int y, int targetBoard[][WIDTH], int shotBoard[][WIDTH]) {
     if (targetBoard[y][x] != 0) {
         shotBoard[y][x] = 1;  //Hit
@@ -65,11 +91,20 @@ int shoot(int x, int y, int targetBoard[][WIDTH], int shotBoard[][WIDTH]) {
     return 0;
 }
 
+/*
+ * Checks if the ship with the given id is sunk or not by comparing the target to current hits
+ * Returns true if sunk, else false
+*/
 bool isSunk(int id, Ship ships[], int shotBoard[][WIDTH]) {
     //Check if the shot at the ship's position has been hit
     return shotBoard[ships[id].y][ships[id].x] == 1;
 }
 
+/*
+ * Counts the number of ships left (maybe check isSunk on every ship id)
+ * Can be used to check for a win or as output after a ship is sunk
+ * Returns the number of ships remaining
+*/
 int countShipsLeft(Ship ships[], int shotBoard[][WIDTH]) {
     int shipsLeft = 0;
     for (int i = 0; i < NUM_SHIPS; i++) {
