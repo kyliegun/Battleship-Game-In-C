f79a32783ed87c08dd3a762a5a4225a305b67057
Lukas_Buehlmann <sakulb05@gmail.com>
2024-12-02 14:20:48 -0500

    added a second difficulty level

--- opponentAI.c
+++ opponentAI.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "opponentAI.h"
 #include "game.h"
 
@@ -38,6 +39,57 @@ Pos *freeSpace(int shotBoard[][WIDTH], int *size) {
     return coords;
 }
 
+Pos *findHitShipCoords(int shotBoard[][WIDTH], int *size, Ship ships[]) {
+    Pos *coords = malloc(sizeof(Pos) * HEIGHT*WIDTH);
+    if (coords == NULL) {
+        return NULL;
+    }
+    bool shipSunk;
+    int n = 0;
+    Ship ship;
+    int hit;
+    int hitCount;
+
+    int longestShip = 0;
+    for (int i = 0;i < NUM_SHIPS;i++) {
+        if (ships[i].length > longestShip) {
+            longestShip = ships[i].length;
+        }
+    }
+
+    Pos shipCoords[longestShip];
+    Pos shipPiece;
+
+    // loop through each ship and find coordinates of ships that are hit but the ship is not sunk
+    for (int i = 0;i < NUM_SHIPS;i++) {
+        ship = ships[i];
+        hitCount = 0;
+        
+        for (int j = 0; j < ship.length; j++) {
+            shipPiece.y = ship.headpos.y + i*ship.isVertical;
+            shipPiece.x = ship.headpos.x + i*(!ship.isVertical);
+
+            hit = shotBoard[shipPiece.y][shipPiece.x];
+            if (hit == 1) {
+                shipCoords[j].x = shipPiece.x;
+                shipCoords[j].y = shipPiece.y;
+                hitCount++;
+            }
+        }
+
+        if (0 < hitCount && hitCount < ship.length) {
+            for (int i=0;i < ship.length;i++) {
+                coords[n] = shipCoords[i];  // if stuff goes wrong check this for assign by ref
+                n++;
+            }
+        }
+    }
+
+    coords = realloc(coords, sizeof(Pos) * n);
+    *size = n;
+    return coords;
+}
+
 /**
  * @brief The easiest opponent difficulty. Shoots completely randomly
  * 
@@ -51,14 +103,13 @@ Pos easyMode(int shotBoard[][WIDTH]) {
 
     if (coordArray == NULL) {
         printf("No available shots. Something has gone wrong");
-        return;
+    } else {
+        // chooses a random position within the available spaces
+        int randNum = rand() % (size + 1);
+        target = *(coordArray + randNum);
+        free(coordArray);
+        return target;
     }
-
-    // chooses a random position within the available spaces
-    int randNum = rand() % (size + 1);
-    target = *(coordArray + randNum);
-    free(coordArray);
-    return target;
 }
 
 /**
@@ -67,10 +118,10 @@ Pos easyMode(int shotBoard[][WIDTH]) {
  * @param shotBoard - the 2D array of current opponent shots
  * @return Pos - The position to shoot at
  */
-Pos mediumMode(int shotBoard[][WIDTH]) {
+Pos mediumMode(int shotBoard[][WIDTH], Ship ships[]) {
     int size;
     Pos target;
-    Pos *coordArray = freeSpace(shotBoard, &size);
+    Pos *coordArray = findHitShipCoords(shotBoard, &size, ships);
 
     if (coordArray == NULL) {
         printf("No available shots. Something has gone wrong");
@@ -81,6 +132,39 @@ Pos mediumMode(int shotBoard[][WIDTH]) {
     int randNum = rand() % (size + 1);
     target = *(coordArray + randNum);
     free(coordArray);
+    coordArray = NULL;
+
+    // choose a random shift direction
+    randNum = randNum % 4;
+    switch (randNum) {
+        case 0:
+            // left side of target
+            if (target.x > 0 && shotBoard[target.y][target.x-1] != -1) {
+                target.x--;
+                return target;
+            }
+        case 1:
+            // right side of target
+            if (target.x < WIDTH - 1 && shotBoard[target.y][target.x+1] != -1) {
+                target.x++;
+                return target;
+            }
+        case 2:
+            // top side of target
+            if (target.y > 0 && shotBoard[target.y-1][target.x] != -1) {
+                target.y--;
+                return target;
+            }
+        case 3:
+            // bottom side of target
+            if (target.y < HEIGHT - 1 && shotBoard[target.y+1][target.x] != -1) {
+                target.y++;
+                return target;
+            }
+        default:
+            printf("Something went wrong. No valid target found");
+
+    }
     return target;
 }
 
@@ -92,11 +176,16 @@ Pos mediumMode(int shotBoard[][WIDTH]) {
  * @param difficulty - the difficulty level. 1=easy
  * @param shotBoard - the 2D array representing the places that have been shot at
  */
-void opponentShoot(int *x, int *y, int difficulty, int shotBoard[][WIDTH]) {
+void opponentShoot(int *x, int *y, int difficulty, int shotBoard[][WIDTH], Ship ships[]) {
     Pos target;
     switch (difficulty) {
         case 1:
             target = easyMode(shotBoard);
+            *x = target.x;
+            *y = target.y;
+            break;
+        case 2:
+            target = mediumMode(shotBoard, ships);
             *x = target.x;
             *y = target.y;
             break;
--- opponentAI.h
+++ opponentAI.h
@@ -1,7 +1,8 @@
 #include "game.h"
 
-void opponentShoot(int *x, int *y, int difficulty, int shotBoard[][WIDTH]);
+void opponentShoot(int *x, int *y, int difficulty, int shotBoard[][WIDTH], Ship ships[]);
 Pos *freeSpace(int shotBoard[][WIDTH], int *size);
+Pos *findHitShipCoords(int shotBoard[][WIDTH], int *size, Ship ships[]);
 
 Pos easyMode(int shotBoard[][WIDTH]);
-Pos mediumMode(int shotBoard[][WIDTH]);
+Pos mediumMode(int shotBoard[][WIDTH], Ship ships[]);
